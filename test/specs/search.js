3735523177ff8cb1fe9f324b770aee41c5278da9
Natalie Reynolds <92003973+natvrey@users.noreply.github.com>
2024-02-03 22:10:11 -0500

    update config to generate allure-report folder

--- test/specs/search.js
+++ test/specs/search.js
@@ -1,18 +1,17 @@
-import HomePage from '../pageobjects/home.page.js'
-import { searchData } from '../search.data.js';
+import HomePage from "../pageobjects/home.page.js";
+import { searchData } from "../search.data.js";
 
 const { queryOne } = searchData.search;
 
-describe('YouTube - Search for a video', () => {
-    it('should search for a video', async () => {
-        await HomePage.open()
-        await HomePage.search(queryOne)
-    })
-})
+describe("YouTube - Search for a video", () => {
+  it("Should search for a video", async () => {
+    await HomePage.open();
+    await HomePage.search(queryOne);
+  });
+});
 
-describe('YouTube - Click on desired video', () => {
-    it('should click on desired video', async () => {
-        await HomePage.chooseVideo()
-    })
-})
-
+describe("YouTube - Click on desired video", () => {
+  it("Should click on desired video", async () => {
+    await HomePage.chooseVideo();
+  });
+});
